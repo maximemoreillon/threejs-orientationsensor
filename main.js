8f345515b8e19682208a023f3672cc401ff394b7
Maxime Moreillon <m.moreillon@gmail.com>
2022-09-26 19:18:33 +0900

    model and permissions

--- main.js
+++ main.js
@@ -15,8 +15,26 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const mesh = new THREE.Mesh(geometry, material)
+
+let model
+// scene.add(mesh);
+
+const loader = new FBXLoader()
+loader.setPath('./')
+loader.load('arrow.fbx', (fbx) => {
+
+  model = fbx
+
+  model.scale.setScalar(0.001)
+  model.position.set(0, 0, 0)
+  model.traverse(c => {
+    //c.castShadow = true
+    c.material = new THREE.MeshNormalMaterial()
+  })
+
+  scene.add(model)
+})
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,25 +44,52 @@ document.body.appendChild(renderer.domElement);
 // animation
 
 function animation(time) {
-
-
   renderer.render(scene, camera);
+}
 
+
+// Sensors API
+
+try {
+  const options = { frequency: 60, referenceFrame: "device" };
+  const sensor = new AbsoluteOrientationSensor(options);
+
+  // Reading event
+  sensor.addEventListener("reading", () => {
+    //model.quaternion.fromArray(sensor.quaternion).inverse();
+    model.quaternion.fromArray(sensor.quaternion).conjugate()
+  });
+
+  // Error event
+  sensor.addEventListener("error", (error) => {
+    console.error(error)
+  });
+
+  const startButton = document.getElementById('startbutton')
+
+  console.log(startButton)
+
+  startButton.addEventListener('click', async () => {
+
+    const results = await Promise.all([
+      navigator.permissions.query({ name: "accelerometer" }),
+      navigator.permissions.query({ name: "magnetometer" }),
+      navigator.permissions.query({ name: "gyroscope" }),
+    ])
+
+    if (results.every((result) => result.state === "granted")) {
+      sensor.start();
+    } 
+    else {
+      console.log("No permissions to use AbsoluteOrientationSensor.");
+    }
+
+  })
+
+}
+catch (error) {
+  console.error(`Sensors API not available`)
 }
 
 
 
-const options = { frequency: 60, referenceFrame: "device" };
-const sensor = new AbsoluteOrientationSensor(options);
-
-sensor.addEventListener("reading", () => {
-  // model is a Three.js object instantiated elsewhere.
-  //mesh.quaternion.fromArray(sensor.quaternion).inverse();
-  mesh.quaternion.fromArray(sensor.quaternion).conjugate()
-});
-sensor.addEventListener("error", (error) => {
-  if (error.name === "NotReadableError") {
-    console.log("Sensor is not available.");
-  }
-});
-sensor.start();
